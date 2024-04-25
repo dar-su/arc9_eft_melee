bc42988aca2879e55437cf549cdd5e025ee6da25
Darsu <95997959+dar-su@users.noreply.github.com>
2024-04-25 19:03:29 +0700

    kukri

---
+++ lua/weapons/arc9_eft_melee_kukri.lua
@@ -0,0 +1,70 @@
+SWEP.Base = "arc9_eft_melee_base"
+SWEP.Category = "ARC9 - Escape From Tarkov"
+SWEP.SubCategory = ARC9:GetPhrase("eft_subcat_melee")
+SWEP.Spawnable = true
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "Kukri"
+SWEP.Class = ARC9:GetPhrase("eft_class_weapon_melee")
+SWEP.Description = [[to get this knife bsg wants you to pay 250 euro. isnt it insane?
+i dont have real description rn or stats]]
+
+SWEP.ViewModel = "models/weapons/arc9/darsu_eft/c_melee_kukri.mdl"
+SWEP.WorldModel = "models/weapons/arc9/darsu_eft/w_melee_kukri.mdl"
+
+SWEP.DamageMax = 25
+
+SWEP.BashDamage = 25
+SWEP.BashLungeRange = 0
+SWEP.BashRange = 0.6 / 0.0254 * 2
+SWEP.PreBashTime = 0.3
+SWEP.PostBashTime = 0.4
+
+SWEP.Bash2Damage = 27
+SWEP.Bash2LungeRange = 0
+SWEP.Bash2Range = 0.7 / 0.0254 * 2
+SWEP.PreBash2Time = 0.45
+SWEP.PostBash2Time = 0.5
+
+SWEP.CustomizeAng = Angle(87, -25, -94.3)
+SWEP.CustomizePos = Vector(-10, 22, 16)
+SWEP.CustomizeSnapshotFOV = 75
+SWEP.CustomizeRotateAnchor = Vector(19, -4.28, -5.23)
+
+SWEP.Animations = {
+    ["idle"] = {
+        Source = "idle",
+    },
+
+    ["draw"] = {
+        Source = {"draw", "draw2"},
+        EventTable = {
+            { s = "weapons/darsu_eft/melee/knife_bayonet_equip.ogg", t = 0.25 },
+        }
+    },
+
+    ["holster"] = {
+        Source = "holster",
+        EventTable = {
+            { s = "weapons/darsu_eft/melee/knife_bayonet_holster.ogg", t = 0.25 },
+        }
+    },
+
+    ["inspect"] = {
+        Source = "inspect"
+    },
+    
+    ["bash"] = {
+        Source = "fire1"
+    },
+    ["bash2"] = {
+        Source = "fire2"
+    },
+
+    ["impact"] = {
+        Source = "fire1_hit"
+    },
+    ["impact2"] = {
+        Source = "fire2_hit"
+    },
+}
